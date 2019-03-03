a9d8b8f71093b344dee07d247887b17ba8a4a967
MGlauer <martinglauer89@gmail.com>
2019-03-03 14:53:59 +0100

    Implement reader for vampire

---
+++ src/gavel/prover/vampire/reader.py
@@ -0,0 +1,16 @@
+import re
+
+proof_line_regex = re.compile(r'^\d+\. .* \[(?:input (?P<input>.*)|(.*))\]$')
+
+def read_proof(path):
+    with open(path, 'r') as f:
+        for line in f.readlines():
+            if line[0] not in ["%", "\n"]:
+                match = re.match(proof_line_regex, line)
+                if match:
+                    inp = match.groupdict().get('input')
+                    if inp is not None:
+                        print(inp)
+                else:
+                    raise Exception("Not matched:" + line)
+
