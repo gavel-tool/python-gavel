ad7fffe4c4fb68c9f19c55af9f2cd8586fb29077
MGlauer <martinglauer89@gmail.com>
2019-04-15 14:49:43 +0200

    Move test files to separate folder

--- tests/test_logic.py
+++ tests/test_logic.py
@@ -6,7 +6,7 @@ import unittest
 class TestFOL(unittest.TestCase):
     def test_symbols(self):
         processor = Processor()
-        result = list(processor.load_expressions_from_file('single_line_fof.txt'))
+        result = list(processor.load_expressions_from_file('files/single_line_fof.txt'))
         self.assertEqual(len(result), 1, "Single line was not parsed as single line")
         line, _ = result[0]
         self.assertSetEqual(set(line.symbols()), {'X0', 'X1', 'X2', 'X3', 'ismeet', 'leq'})
