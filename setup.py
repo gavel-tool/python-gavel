e39b4716a36f4c30a3842cbe9877dc08e4adbc55
MGlauer <martinglauer89@gmail.com>
2019-04-15 17:40:09 +0200

    Configure travis to use antlr

--- setup.py
+++ setup.py
@@ -116,6 +116,9 @@ setup(
         # eg: 'keyword1', 'keyword2', 'keyword3',
     ],
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    setup_requires=[
+        "setuptools-antlr"
+    ],
     install_requires=[
         # eg: 'aspectlib==1.1.1', 'six>=1.7',
         "click",
