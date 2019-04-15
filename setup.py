c371bfb6ad9bbb98b07a166d08ba1d464126fccf
MGlauer <martinglauer89@gmail.com>
2019-04-15 18:40:11 +0200

    Call antlr4 directly in travis setup
    setuptools-antlr seems to be broken...

--- setup.py
+++ setup.py
@@ -117,7 +117,6 @@ setup(
     ],
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
     setup_requires=[
-        "setuptools-antlr"
     ],
     install_requires=[
         # eg: 'aspectlib==1.1.1', 'six>=1.7',
