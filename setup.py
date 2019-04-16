73aaec50d03f91313150ea8c507d5d4c1e7efb44
MGlauer <martinglauer89@gmail.com>
2019-04-16 10:51:37 +0200

    Remove obsolete setup_requires info

--- setup.py
+++ setup.py
@@ -116,8 +116,6 @@ setup(
         # eg: 'keyword1', 'keyword2', 'keyword3',
     ],
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
-    setup_requires=[
-    ],
     install_requires=[
         # eg: 'aspectlib==1.1.1', 'six>=1.7',
         "click",
