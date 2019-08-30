fb5431aa87025a910cdaf7d4e5e560a9adb9dab6
MGlauer <martinglauer89@gmail.com>
2019-08-30 15:41:21 +0200

    Implement parser and compiler for json-structure in db

--- src/gavel/dialects/db/compiler.py
+++ src/gavel/dialects/db/compiler.py
@@ -4,67 +4,82 @@ from gavel.dialects.base.compiler import Compiler
 
 class DBCompiler(Compiler):
     def visit_quantifier(self, quantifier: fol.Quantifier):
-        raise NotImplementedError
+        if quantifier.is_existential():
+            return dict(type="quantifier", quantifier="existential")
+        elif quantifier.is_universal():
+            return dict(type="quantifier", quantifier="universial")
 
     def visit_formula_role(self, role: fol.FormulaRole):
+        if role == fol.FormulaRole.ASSUMPTION:
+            return dict(type="formula_role", formula_role="assumption")
+        elif role == fol.FormulaRole.AXIOM:
+            return dict(type="formula_role", formula_role="axiom")
+        elif role == fol.FormulaRole.CONJECTURE:
+            return dict(type="formula_role", formula_role="conjecture")
+        elif role == fol.FormulaRole.COROLLARY:
+            return dict(type="formula_role", formula_role="corollary")
+        elif role == fol.FormulaRole.DEFINITION:
+            return dict(type="formula_role", formula_role="definition")
+        elif role == fol.FormulaRole.FI_DOMAIN:
+            return dict(type="formula_role", formula_role="fi_domain")
+        elif role == fol.FormulaRole.FI_FUNCTORS:
+            return dict(type="formula_role", formula_role="fi_functors")
+        elif role == fol.FormulaRole.FI_PREDICATES:
+            return dict(type="formula_role", formula_role="fi_predicates")
+        elif role == fol.FormulaRole.HYPOTHESIS:
+            return dict(type="formula_role", formula_role="hypothesis")
+        elif role == fol.FormulaRole.LEMMA:
+            return dict(type="formula_role", formula_role="lemma")
+        elif role == fol.FormulaRole.NEGATED_CONJECTURE:
+            return dict(type="formula_role", formula_role="negated_conjecture")
+        elif role == fol.FormulaRole.PLAIN:
+            return dict(type="formula_role", formula_role="plain")
+        elif role == fol.FormulaRole.THEOREM:
+            return dict(type="formula_role", formula_role="theorem")
+        elif role == fol.FormulaRole.TYPE:
+            return dict(type="formula_role", formula_role="type")
+        elif role == fol.FormulaRole.UNKNOWN:
+            return dict(type="formula_role", formula_role="unknown")
         raise NotImplementedError
 
     def visit_binary_connective(self, connective: fol.BinaryConnective):
-        raise NotImplementedError
+        return dict(type="binary_connective", binary_connective=connective.name.lower())
 
     def visit_defined_predicate(self, predicate: fol.DefinedPredicate):
-        raise NotImplementedError
+        return dict(type="defined_predicate", defined_predicate=predicate.name.lower())
 
-    def visit_unary_connective(self, predicate: fol.UnaryConnective):
-        raise NotImplementedError
+    def visit_unary_connective(self, connective: fol.UnaryConnective):
+        return dict(type="unary_connective", unary_connective=connective.name.lower())
 
     def visit_unary_formula(self, formula: fol.UnaryFormula):
-        raise NotImplementedError
+        return dict(type="unary_formula", formula=self.visit(formula.formula), connective=self.visit(formula.connective))
 
     def visit_quantified_formula(self, formula: fol.QuantifiedFormula):
-        raise NotImplementedError
+        return dict(type="quantified_formula", formula=self.visit(formula.formula), quantifier=self.visit(formula.quantifier), variables=[self.visit(v) for v in formula.variables])
 
     def visit_annotated_formula(self, anno: fol.AnnotatedFormula):
-        raise NotImplementedError
+        return dict(type="annotated_formula", formula=self.visit(anno.formula), name=self.visit(anno.name), role=self.visit(anno.role), logic=self.visit(anno.logic))
 
     def visit_binary_formula(self, formula: fol.BinaryFormula):
-        raise NotImplementedError
+        return dict(type="binary_formula", left=self.visit(formula.left), right=self.visit(formula.right), connective=self.visit(formula.operator))
 
     def visit_functor_expression(self, expression: fol.FunctorExpression):
-        raise NotImplementedError
+        return dict(type="functor_expression", arguments=[self.visit(a) for a in expression.arguments], functor=self.visit(expression.functor))
 
     def visit_predicate_expression(self, expression: fol.PredicateExpression):
-        raise NotImplementedError
-
-    def visit_typed_variable(self, variable: fol.TypedVariable):
-        raise NotImplementedError
-
-    def visit_type_formula(self, formula: fol.TypeFormula):
-        raise NotImplementedError
+        return dict(type="predicate_expression", arguments=[self.visit(a) for a in expression.arguments], predicate=self.visit(expression.predicate))
 
     def visit_conditional(self, conditional: fol.Conditional):
-        raise NotImplementedError
-
-    def visit_let(self, expression: fol.Let):
-        raise NotImplementedError
-
-    def visit_subtype(self, expression: fol.Subtype):
-        raise NotImplementedError
-
-    def visit_quantified_type(self, expression: fol.QuantifiedType):
-        raise NotImplementedError
+        return dict(type="conditional", if_clause=conditional.if_clause, then_clause=conditional.then_clause, else_clause=conditional.else_clause)
 
     def visit_import(self, imp: fol.Import):
-        raise NotImplementedError
-
-    def visit_mapping_type(self, expression: fol.Subtype):
-        raise NotImplementedError
+        return dict(type="import", path=imp.path)
 
     def visit_variable(self, variable: fol.Variable):
-        raise NotImplementedError
+        return dict(type="variable", symbol=variable.symbol)
 
     def visit_constant(self, variable: fol.Variable):
-        raise NotImplementedError
+        return dict(type="constant", symbol=variable.symbol)
 
     def visit_problem(self, problem: fol.Problem):
-        raise NotImplementedError
+        return dict(type="problem", premises=[self.visit(p) for p in problem.premises], conjecture=self.visit(problem.conjecture))
--- src/gavel/dialects/db/parser.py
+++ src/gavel/dialects/db/parser.py
@@ -32,3 +32,87 @@ class DBProblemParser(ProblemParser):
                     premises.append(s)
         for c in conjectures:
             yield Problem(premises, c)
+
+class DBFOLParser(LogicParser):
+
+    def _parse_rec(self, obj, *args, **kwargs):
+        if isinstance(obj, str):
+            return obj
+        meth = getattr(self, "parse_%s" % obj.get("type"), None)
+
+        if meth is None:
+            raise Exception(
+                "Parser '{name}' not found for {cls}".format(
+                    name=obj.get("type"), cls=obj
+                )
+            )
+        return meth(obj)
+
+    def parse_quantifier(self, quantifier: dict):
+        if quantifier.get("type") == "existential":
+            q = fol.Quantifier.EXISTENTIAL
+        elif quantifier.get("type") == "universial":
+            q = fol.Quantifier.UNIVERSAL
+        else:
+            raise NotImplementedError
+        return dict(type="quantifier", quantifier=q)
+
+    def parse_formula_role(self, role: dict):
+        return getattr(fol.FormulaRole, role["formula_role"].upper())
+
+    def parse_binary_connective(self, connective: dict):
+        return getattr(fol.BinaryConnective, connective["binary_connective"].upper())
+
+    def parse_defined_predicate(self, predicate: dict):
+        return getattr(fol.DefinedPredicate, predicate["defined_predicate"].upper())
+
+    def parse_unary_connective(self, connective: dict):
+        return getattr(fol.UnaryConnective, connective["unary_connective"].upper())
+
+    def parse_unary_formula(self, formula: dict):
+        return fol.UnaryFormula(formula=self._parse_rec(formula["formula"]), connective=self._parse_rec(formula["connective"]))
+
+    def parse_quantified_formula(self, formula: dict):
+        return fol.QuantifiedFormula(
+            formula=self._parse_rec(formula["formula"]),
+            quantifier=self._parse_rec(formula["quantifier"]),
+            variables=[self._parse_rec(v) for v in formula["quantifier"]])
+
+    def parse_annotated_formula(self, anno: dict):
+        return fol.AnnotatedFormula(
+            formula=self._parse_rec(anno["formula"]),
+            name=anno["name"],
+            role=self._parse_rec(anno["role"]),
+            logic=anno["logic"],
+        )
+
+    def parse_binary_formula(self, formula: dict):
+        return fol.BinaryFormula(
+            left=self._parse_rec(formula["left"]),
+            right=self._parse_rec(formula["right"]),
+            operator=self._parse_rec(formula["connective"]))
+
+    def parse_functor_expression(self, expression: fol.FunctorExpression):
+        return fol.FunctorExpression(
+            functor=self._parse_rec(expression["functor"]),
+            arguments=[self._parse_rec(a) for a in expression["arguments"]])
+
+    def parse_predicate_expression(self, expression: dict) -> fol.PredicateExpression:
+        return fol.PredicateExpression(
+            predicate=self._parse_rec(expression["functor"]),
+            arguments=[self._parse_rec(a) for a in expression["arguments"]])
+
+    def parse_conditional(self, conditional: dict) -> fol.Conditional:
+        return fol.Conditional(if_clause=self._parse_rec(conditional["if_clause"]), then_clause=self._parse_rec(conditional["then_clause"]), else_clause=self._parse_rec(conditional["else_clause"]))
+
+    def parse_import(self, imp: dict) -> fol.Import:
+        return fol.Import(path=imp["path"])
+
+    def parse_variable(self, variable: dict) -> fol.Variable:
+        return fol.Variable(symbol=variable["symbol"])
+
+    def parse_constant(self, variable: dict):
+        return fol.Constant(symbol=variable["symbol"])
+
+    def parse_problem(self, problem: dict) -> fol.Problem:
+        return fol.Problem(premises=[self._parse_rec(p) for p in problem["premises"]], conjecture=self._parse_rec(problem["conjecture"]))
