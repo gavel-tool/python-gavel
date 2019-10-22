80a9b647ece6e0b2e4a3e17e936ea47ea1f21478
MGlauer <martinglauer89@gmail.com>
2019-10-22 14:43:35 +0200

    Make DefinedConstant pickleable

--- src/gavel/logic/logic.py
+++ src/gavel/logic/logic.py
@@ -318,7 +318,7 @@ class DistinctObject(LogicElement):
         return {self.symbol}
 
 
-class DefinedConstant(Constant, Enum):
+class DefinedConstant(Enum):
 
     __visit_name__ = "defined_constant"
 
